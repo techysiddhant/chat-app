ff6decd58bad563b042b8195544039afcdb0f2f5
techysiddhant <sidjain8000@gmail.com>
2024-09-12 19:10:09 +0530

    added basic frontend with auth and server setup

---
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,81 @@
+import { Account, AuthOptions, ISODateString } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import GoogleProvider from "next-auth/providers/google";
+import axios, { AxiosError } from "axios";
+import { LOGIN_URL } from "@/lib/apiAuthRoutes";
+import { redirect } from "next/navigation";
+
+export interface CustomSession {
+  user?: CustomUser;
+  expires: ISODateString;
+}
+export interface CustomUser {
+  id?: string | null;
+  name?: string | null;
+  email?: string | null;
+  image?: string | null;
+  provider?: string | null;
+  token?: string | null;
+}
+export const authOptions: AuthOptions = {
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async signIn({
+      user,
+      account,
+    }: {
+      user: CustomUser;
+      account: Account | null;
+    }) {
+      try {
+        const payload = {
+          email: user.email!,
+          name: user.name!,
+          oauth_id: account?.providerAccountId as string,
+          provider: account?.provider as string,
+          image: user?.image,
+        };
+        const { data } = await axios.post(LOGIN_URL, payload);
+
+        user.id = data?.user?.id?.toString();
+        user.token = data?.user?.token;
+        return true;
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          return redirect(`/auth/error?message=${error?.message}`);
+        }
+        return redirect(
+          `/auth/error?message=Something went wrong.please try again!`
+        );
+      }
+    },
+
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+
+    async session({ session, token }: { session: CustomSession; token: JWT }) {
+      session.user = token.user as CustomUser;
+      return session;
+    },
+  },
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+};
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,28 @@
+import FeatureSection from "@/components/base/FeatureSection";
+import Footer from "@/components/base/Footer";
+import HeroSection from "@/components/base/HeroSection";
+import Navbar from "@/components/base/Navbar";
+import UserReviews from "@/components/base/UserReviews";
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session: CustomSession | null = await getServerSession(authOptions);
+  return (
+    <div className="min-h-screen flex flex-col ">
+      {/* Header */}
+      <Navbar user={session?.user ?? null} />
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}

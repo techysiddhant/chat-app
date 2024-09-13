a7819182d9813d739b4dd4b3438cc884de5f494b
techysiddhant <sidjain8000@gmail.com>
2024-09-13 21:46:44 +0530

    add prisma

---
+++ server/prisma/migrations/20240913161311_users_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(191) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "provider" VARCHAR(100) NOT NULL,
+    "image" TEXT,
+    "oauth_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
